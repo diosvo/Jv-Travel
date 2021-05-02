8adb28286340eef286a5169c28dfdc5a78aee5e5
Dios Vo <vtmn1212@gmail.com>
2021-05-03 01:08:44 +0700

    Create Cart (pojo, api, controller)

--- src/main/java/com/dv/controllers/HomeController.java
+++ src/main/java/com/dv/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package com.dv.controllers;
 
-import com.dv.pojo.Destination;
-import com.dv.pojo.Product;
+import com.dv.pojo.Cart;
 import com.dv.service.DestinationService;
 import com.dv.service.ProductService;
-import java.util.List;
+import com.dv.utils.Utils;
+import java.math.BigDecimal;
+import java.util.Map;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,9 +29,12 @@ public class HomeController {
     private DestinationService destinationService;
 
     @ModelAttribute
-    public void addAttribute(Model model) {
+    public void addAttribute(Model model, HttpSession session) {
         model.addAttribute("products", this.productService.getProducts(""));
         model.addAttribute("destinations", this.destinationService.getDestinations());
+
+        Map<String, BigDecimal> cartStats = Utils.cartStats((Map<Integer, Cart>) session.getAttribute("cart"));
+        model.addAttribute("cartStats", cartStats);
     }
 
     @RequestMapping("/")
---
+++ src/main/java/com/dv/pojo/Cart.java
@@ -0,0 +1,87 @@
+package com.dv.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author diosvo
+ */
+public class Cart {
+
+    private int productId;
+    private String tourName;
+    private String image;
+    private BigDecimal price;
+    private int quantity;
+
+    /**
+     * @return the productId
+     */
+    public int getProductId() {
+        return productId;
+    }
+
+    /**
+     * @param productId the productId to set
+     */
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * @return the tourName
+     */
+    public String getTourName() {
+        return tourName;
+    }
+
+    /**
+     * @param tourName the tourName to set
+     */
+    public void setTourName(String tourName) {
+        this.tourName = tourName;
+    }
+
+    /**
+     * @return the image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
